ef0a2b3cb3736f9c34e0162aef8756d276987acd
Ashik465 <helloashikul@gmail.com>
2023-08-28 13:38:16 +0600

    navbar-half-done

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
+import Navbar from '@/components/navbar/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main >
+    <main  >
+
+      
            
-        this is the main page 
+       
     </main>
   )
 }
