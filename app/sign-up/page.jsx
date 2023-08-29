033d71be237e7863bfca8a06dc4701fa40eed2bd
Ashik465 <helloashikul@gmail.com>
2023-08-29 17:28:27 +0600

    formating

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,24 +1,31 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const page = () => {
   const initialValues = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   };
 
   const validationSchema = Yup.object({
-    name: Yup.string().required('Name is Required'),
-    email: Yup.string().email('Invalid email format').required('Email is Required'),
-    password: Yup.string().required('Password is Required').min(6, 'Password must be at least 6 characters'),
+    name: Yup.string().required("Name is Required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is Required"),
+    password: Yup.string()
+      .required("Password is Required")
+      .min(6, "Password must be at least 6 characters"),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Confirm password  must match Passwords ')
-      .required('confirm password is Required'),
+      .oneOf(
+        [Yup.ref("password"), null],
+        "Confirm password  must match Passwords "
+      )
+      .required("confirm password is Required"),
   });
 
   const handleSubmit = (values, { resetForm }) => {
@@ -30,33 +37,102 @@ const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full md:w-1/2 lg:w-1/3">
-        <h2 className="text-2xl font-bold flex items-center justify-center mb-4">Sign Up</h2>
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        <h2 className="text-2xl font-bold flex items-center justify-center mb-4">
+          Sign Up
+        </h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
           <Form>
             <div className="mb-4">
-              <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-              <Field type="text" id="name" name="name" placeholder = 'Name' className="mt-1 p-2 w-full border rounded-md" />
-              <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
             </div>
             <div className="mb-4">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-              <Field type="email" id="email" name="email" placeholder = 'Email'   className="mt-1 p-2 w-full border rounded-md" />
-              <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
             </div>
             <div className="mb-4">
-              <label htmlFor="password"  className="block text-sm font-medium text-gray-700">Password</label>
-              <Field type="password" placeholder = 'Password' id="password" name="password" className="mt-1 p-2 w-full border rounded-md" />
-              <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <Field
+                type="password"
+                placeholder="Password"
+                id="password"
+                name="password"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
             </div>
             <div className="mb-4">
-              <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
-              <Field type="password" placeholder = ' Confirm Password' id="confirmPassword" name="confirmPassword" className="mt-1 p-2 w-full border rounded-md" />
-              <ErrorMessage name="confirmPassword" component="div" className="text-red-500 text-sm mt-1" />
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <Field
+                type="password"
+                placeholder=" Confirm Password"
+                id="confirmPassword"
+                name="confirmPassword"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+              <ErrorMessage
+                name="confirmPassword"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
             </div>
-           
-               <button type="submit" className="text-white p-2 rounded-md w-full bg-red-600 hover:bg-black ">Sign Up</button> 
-          
-            
+
+            <button
+              type="submit"
+              className="text-white p-2 rounded-md w-full bg-red-600 hover:bg-black "
+            >
+              Sign Up
+            </button>
           </Form>
         </Formik>
       </div>
