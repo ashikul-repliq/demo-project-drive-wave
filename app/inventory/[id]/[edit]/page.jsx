5b2d51990520604d0b7b8e7227253c9850db4e40
Ashik465 <helloashikul@gmail.com>
2023-09-07 10:59:59 +0600

    data shown on edit page

--- app/inventory/[id]/[edit]/page.jsx
+++ app/inventory/[id]/[edit]/page.jsx
@@ -1,9 +1,53 @@
 'use client'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
-const page = ({params}) => {
+const EditPage = ({params}) => {
 
-    console.log(params);
+  // fetch function
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(
+        `https://64f038f18a8b66ecf7794bb9.mockapi.io/products/${params.id}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      return []; // Return an empty array in case of an error
+    }
+  };
+
+  // use tanstake query to get all products
+  const { data, isLoading } = useQuery({
+    queryKey: ["single-product", params.id],
+    queryFn: fetchProducts,
+  });
+
+  // console.log(data);
+  const {
+    Img,
+    Condition,
+    Name,
+    Views,
+    ListedOn,
+    createdAt,
+    Mileage,
+    Transmission,
+    Year,
+    FuelType,
+    Color,
+    Doors,
+    Cylinders,
+    VIN,
+    EngineSize,
+    Details,
+    CarFeatures,
+    VehicleHistory,
+    Reviews, id
+  } = data || {};
+  console.log(data);
     return (
         <div>
             edit
@@ -11,4 +55,4 @@ const page = ({params}) => {
     );
 };
 
-export default page;
+export default EditPage;
