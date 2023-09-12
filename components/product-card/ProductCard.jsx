f2e85971d3b0f29b070f12bf027111527874101e
ashikul-repliq <ashikul.islam@repliq.dev>
2023-09-12 14:51:53 +0600

    delete-button-updated

--- components/product-card/ProductCard.jsx
+++ components/product-card/ProductCard.jsx
@@ -53,14 +53,7 @@ const ProductCard = ({ product, refetch }) => {
   return (
     <div className="relative">
       {/* // product card */}
-      {admin && (
-        <div
-          onClick={deleteProduct}
-          className="absolute text-4xl text-red-600 hover:text-red-800 top-36 right-4 z-50"
-        >
-          <MdDelete></MdDelete>
-        </div>
-      )}
+
       <div className=" bg-white p-4 rounded-lg shadow-lg  h-min ">
         <div className="relative ">
           <Image
@@ -78,19 +71,43 @@ const ProductCard = ({ product, refetch }) => {
                 New
               </h1>
             ) : (
-              <h1 className="text-white bg-yellow-600 rounded-full px-4 py-2 text-sm inline bg-opacity-80">Used</h1>
+              <h1 className="text-white bg-yellow-600 rounded-full px-4 py-2 text-sm inline bg-opacity-80">
+                Used
+              </h1>
             )}
           </div>
         </div>
 
-        <h1 className="py-2 font-bold text-xl text-black ">{Name} </h1>
+        <div className="flex justify-between items-center">
+          <h1 className="py-2 font-bold text-xl text-black ">{Name} </h1>
+
+          {admin && (
+            <div
+              onClick={deleteProduct}
+              className=" text-4xl text-red-600 hover:text-red-800  z-50"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          )}
+        </div>
         <div className="flex items-center">
           {starIcons}
           <span className="text-gray-400 ml-2"> {Rating}</span>{" "}
           <h1 className="text-gray-400 ml-1">(58.5k Review)</h1>
         </div>
         <div className="flex items-center my-2">
-          <BiCableCar className="text-gray-400"  ></BiCableCar>
+          <BiCableCar className="text-gray-400"></BiCableCar>
           <span className="text-gray-600 mx-2 "> {Transmission}</span>
           <GiRoad className="text-gray-400"></GiRoad>
           <span className="text-gray-600 ml-2"> {Mileage}</span>
@@ -113,10 +130,7 @@ const ProductCard = ({ product, refetch }) => {
             href={`/inventory/${id}`}
             className=" text-white px-4 py-2 rounded-lg bg-gray-900 hover:bg-gray-700 transition duration-300 ease-in-out"
           >
-            <h1 className="flex items-center gap-2 font-bold">
-              {" "}
-              Details
-            </h1>
+            <h1 className="flex items-center gap-2 font-bold"> Details</h1>
           </Link>
         </div>
       </div>
